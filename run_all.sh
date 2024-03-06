792971a42d900d73ce271eeed6a6781c0439dd94
Shrish <148286466+shrishj@users.noreply.github.com>
2024-03-05 21:48:54 -0800

    adding check for run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -7,7 +7,7 @@ PROJECT_NAME="template"
 # Tell user what we're doing
 echo -e "Making \033[35m${PROJECT_NAME}\033[0m with shell: ${SHELL}"
 
-# Run makeiles of each module
+# Run makefiles of each module
 (cd 1_data && ${SHELL} make.sh)
 (cd 2_analysis && ${SHELL} make.sh)
 (cd 3_slides && ${SHELL} make.sh)
