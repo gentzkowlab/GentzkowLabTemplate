cf4817990e928234c4ee03ff7b23425031b4cbf8
Luca Moreno Louzada <lucamlouzada@gmail.com>
2024-11-06 10:54:34 -0800

    #24 change example in get_inputs.sh instructions

--- 2_analysis/get_inputs.sh
+++ 2_analysis/get_inputs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Define your input file paths here
-# Paths may be absolute or relative to the current module. For example, this is a relative path:
-# ../examples/inputs_for_examples/mpg.csv
+# Paths may be absolute or relative, depending on what the current working directory is. 
+# For example, this is a relative path:
+# examples/inputs_for_examples/mpg.csv
 # And this is an absolute path:
 # /Users/username/GentzkowLabTemplate/examples/inputs_for_examples/mpg.csv
 INPUT_FILES=(
